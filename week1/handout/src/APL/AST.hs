f16462188ce6503bacd0ae9aa59942d26c054535
SashimmyUni <jimmyhuynhuni@gmail.com>
2026-09-03 21:25:27 +0200

    Week1 AST exercise

--- week1/handout/src/APL/AST.hs
+++ week1/handout/src/APL/AST.hs
@@ -1,4 +1,18 @@
 module APL.AST
   (
+    Exp(..)
+    Val(..)
   )
 where
+
+data Exp
+  = CstInt Integer
+  deriving (Eq, Show)
+
+data Val
+  = ValInt Integer
+  deriving (Eq, Show)
+
+eval :: Exp -> Val
+eval (CstInt n) = ValInt n
+
